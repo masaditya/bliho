ff4c34a2c7bab789f09ac541e649447ddae75f68
adit <aeka489@gmail.com>
2019-06-23 17:10:51 +0700

    update profile section

--- application/views/frontend/user_dashboard/tab-content.php
+++ application/views/frontend/user_dashboard/tab-content.php
@@ -1,188 +1,342 @@
 <head>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-		integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+	<link
+		rel="stylesheet"
+		href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+		integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+		crossorigin="anonymous"
+	/>
 	<link rel="stylesheet" href="<?=base_url()?>assets/css/css/dashboard.css" />
 	<link rel="stylesheet" href="<?=base_url()?>assets/css/css/style.css" />
-
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
-
 	<div class="container">
 		<div class="d-flex justify-content-center ">
 			<ul class="nav bg-white rounded" role="tablist">
 				<li class="nav-item tab active-tab">
-					<a href="#step-1" id="step1-tab" class="nav-link active" aria-selected="true" data-toggle="tab"
-						role="tab">Profile</a>
+					<a
+						href="#step-1"
+						id="step1-tab"
+						class="nav-link active"
+						aria-selected="true"
+						data-toggle="tab"
+						role="tab"
+						>Profile</a
+					>
 				</li>
 				<li class="nav-item tab">
-					<a href="#step-2" id="step2-tab" class="nav-link " aria-selected="false" data-toggle="tab"
-						role="tab">Following</a>
+					<a
+						href="#step-2"
+						id="step2-tab"
+						class="nav-link "
+						aria-selected="false"
+						data-toggle="tab"
+						role="tab"
+						>Following</a
+					>
 				</li>
 				<li class="nav-item tab">
-					<a href="#step-3" id="step3-tab" class="nav-link " aria-selected="false" data-toggle="tab"
-						role="tab">Setting</a>
+					<a
+						href="#step-3"
+						id="step3-tab"
+						class="nav-link "
+						aria-selected="false"
+						data-toggle="tab"
+						role="tab"
+						>Setting</a
+					>
 				</li>
 				<li class="nav-item tab">
-					<a href="#step-4" id="step4-tab" class="nav-link " aria-selected="false" data-toggle="tab"
-						role="tab">Download</a>
+					<a
+						href="#step-4"
+						id="step4-tab"
+						class="nav-link "
+						aria-selected="false"
+						data-toggle="tab"
+						role="tab"
+						>Download</a
+					>
 				</li>
 				<li class="nav-item tab">
-					<a href="#step-5" id="step5-tab" class="nav-link " aria-selected="false" data-toggle="tab"
-						role="tab">Favorite</a>
+					<a
+						href="#step-5"
+						id="step5-tab"
+						class="nav-link "
+						aria-selected="false"
+						data-toggle="tab"
+						role="tab"
+						>Favorite</a
+					>
 				</li>
 				<li class="nav-item tab">
-					<a href="#step-6" id="step6-tab" class="nav-link " aria-selected="false" data-toggle="tab"
-						role="tab">My Subscription</a>
+					<a
+						href="#step-6"
+						id="step6-tab"
+						class="nav-link "
+						aria-selected="false"
+						data-toggle="tab"
+						role="tab"
+						>My Subscription</a
+					>
 				</li>
 			</ul>
 		</div>
 	</div>
 
 	<div class="container mt-5 tab-content">
-		<div class="tab-pane fade show active" id="step-1" aria-labelledby="step1-tab" role="tabpanel">
+		<div
+			class="tab-pane fade show active"
+			id="step-1"
+			aria-labelledby="step1-tab"
+			role="tabpanel"
+		>
 			<div class="row">
 				<div class="col-md-7">
-					<p class="user-caption">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-						nibh
-						euismod tincidunt ut
-						laoreet dolore magna aliquam erat volutpat</p>
+					<p class="user-caption">
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+						nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+						volutpat
+					</p>
 				</div>
 				<div class="col-md-7">
 					<div class="pl-5 pt-2">
-						<h3 class="font-weight-bold">Account Data </h3>
+						<h3 class="font-weight-bold">Account Data</h3>
 						<div class="form-group w-50">
-							<input class="form-control mt-3" placeholder="Author Name" type="text" name="username"
-								id="username">
-							<input type="email" class="form-control mt-4" name="email" id="email"
-								placeholder="Author@email">
-							<textarea class="form-control mt-4" name="bio" id="bio" rows="3"></textarea>
-							<input class="form-control mt-4" placeholder="Author Country" type="text" name="country"
-								id="country">
-							<input type="password" class="form-control mt-4" placeholder="New Password" name="npassword"
-								id="npassword ">
-							<input type="password" class="form-control mt-4" placeholder="Repeat New Password"
-								name="rpassword" id="rpassword ">
-							<input type="password" class="form-control mt-4" placeholder="Current Password"
-								name="cpassword" id="cpassword ">
+							<input
+								class="form-control mt-3"
+								placeholder="Author Name"
+								type="text"
+								name="username"
+								id="username"
+							/>
+							<input
+								type="email"
+								class="form-control mt-4"
+								name="email"
+								id="email"
+								placeholder="Author@email"
+							/>
+							<textarea
+								class="form-control mt-4"
+								name="bio"
+								id="bio"
+								rows="3"
+							></textarea>
+							<input
+								class="form-control mt-4"
+								placeholder="Author Country"
+								type="text"
+								name="country"
+								id="country"
+							/>
+							<input
+								type="password"
+								class="form-control mt-4"
+								placeholder="New Password"
+								name="npassword"
+								id="npassword "
+							/>
+							<input
+								type="password"
+								class="form-control mt-4"
+								placeholder="Repeat New Password"
+								name="rpassword"
+								id="rpassword "
+							/>
+							<input
+								type="password"
+								class="form-control mt-4"
+								placeholder="Current Password"
+								name="cpassword"
+								id="cpassword "
+							/>
 						</div>
-
 					</div>
 				</div>
 				<div class="col-md-5 text-center ">
-					<p class="bg-dark font-weight-bold text-white mx-auto py-2 w-75">Social and Network
+					<p class="bg-dark font-weight-bold text-white mx-auto py-2 w-75">
+						Social and Network
 					</p>
 					<div class="form-group w-75 mx-auto">
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<span class=" rounded-0 input-group-text" id="basic-addon1"> <i
-										class="fab fa-facebook-f"></i>
+								<span class="rounded-0 px-3 input-group-text" id="basic-addon1">
+									<i class="fab fa-facebook-f"></i>
 								</span>
 							</div>
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" />
 						</div>
 
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<span class=" rounded-0 input-group-text" id="basic-addon1"> <i
-										class="fab fa-twitter"></i>
+								<span class=" rounded-0 input-group-text" id="basic-addon1">
+									<i class="fab fa-twitter"></i>
 								</span>
 							</div>
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" />
 						</div>
 
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<span class=" rounded-0 input-group-text" id="basic-addon1"> <i
-										class="fab fa-instagram"></i>
+								<span class=" rounded-0 input-group-text" id="basic-addon1">
+									<i class="fab fa-instagram"></i>
 								</span>
 							</div>
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" />
 						</div>
 
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<span class=" rounded-0 input-group-text" id="basic-addon1"> <i
-										class="fab fa-dribbble"></i>
+								<span class=" rounded-0 input-group-text" id="basic-addon1">
+									<i class="fab fa-dribbble"></i>
 								</span>
 							</div>
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" />
 						</div>
 
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<span class=" rounded-0 input-group-text" id="basic-addon1"> <i
-										class="fab fa-behance"></i>
+								<span class=" rounded-0 input-group-text" id="basic-addon1">
+									<i class="fab fa-behance"></i>
 								</span>
 							</div>
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" />
 						</div>
 
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<span class=" rounded-0 input-group-text" id="basic-addon1"> <i
-										class="fab fa-pinterest"></i>
+								<span class=" rounded-0 input-group-text" id="basic-addon1">
+									<i class="fab fa-pinterest"></i>
 								</span>
 							</div>
-							<input type="text" class="form-control">
+							<input type="text" class="form-control" />
 						</div>
-
 					</div>
+				</div>
+				<div class="col-md-7 pl-5 mt-5">
+					<h3 class="font-weight-bold">Notifications</h3>
+					<div class="row">
+						<div class="col-sm-10">
+							<li class="list-group-flush user-caption list-unstyled mt-4">
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+								diam nonummy nibh euismod tincidunt ut laoreet.
+							</li>
+							<li class="list-group-flush user-caption list-unstyled mt-4">
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+								diam nonummy nibh euismod tincidunt ut laoreet.
+							</li>
+						</div>
+						<div class="col-sm-2">
+							<label class="switch mt-3">
+								<input type="checkbox" class="success" />
+								<span class="slider round"></span>
+							</label>
+							<label class="switch mt-4">
+								<input type="checkbox" class="success" />
+								<span class="slider round"></span>
+							</label>
+						</div>
+					</div>
+				</div>
 
+				<div class="col-md-6 text-center mt-5 py-5">
+					<button class="btn btn-rounded btn-outline-cyan">
+						Close my account
+					</button>
+				</div>
+				<div class="col-md-6 text-center mt-5 py-5">
+					<button class="btn btn-rounded btn-cyan">
+						Save Changes
+					</button>
 				</div>
 			</div>
+		</div>
 
-
-
-
+		<div
+			class="tab-pane fade"
+			id="step-2"
+			aria-labelledby="step2-tab"
+			role="tabpanel"
+		>
+			Following
 		</div>
-		<div class="tab-pane fade" id="step-2" aria-labelledby="step2-tab" role="tabpanel">Following
+		<div
+			class="tab-pane fade"
+			id="step-3"
+			aria-labelledby="step3-tab"
+			role="tabpanel"
+		>
+			Setting
 		</div>
-		<div class="tab-pane fade" id="step-3" aria-labelledby="step3-tab" role="tabpanel">Setting
+		<div
+			class="tab-pane fade"
+			id="step-4"
+			aria-labelledby="step4-tab"
+			role="tabpanel"
+		>
+			Download
 		</div>
-		<div class="tab-pane fade" id="step-4" aria-labelledby="step4-tab" role="tabpanel">Download
+		<div
+			class="tab-pane fade"
+			id="step-5"
+			aria-labelledby="step5-tab"
+			role="tabpanel"
+		>
+			Favorite
 		</div>
-		<div class="tab-pane fade" id="step-5" aria-labelledby="step5-tab" role="tabpanel">Favorite
-		</div>
-		<div class="tab-pane fade" id="step-6" aria-labelledby="step6-tab" role="tabpanel">My Subscription
+		<div
+			class="tab-pane fade"
+			id="step-6"
+			aria-labelledby="step6-tab"
+			role="tabpanel"
+		>
+			My Subscription
 		</div>
 	</div>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-	</script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-	</script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-	</script>
+	<script
+		src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+		crossorigin="anonymous"
+	></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+		crossorigin="anonymous"
+	></script>
+	<script
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+		crossorigin="anonymous"
+	></script>
 
 	<script>
-		let tab = document.querySelectorAll(".tab")
-		let link = document.querySelectorAll("a[data-toggle='tab']")
+		let tab = document.querySelectorAll(".tab");
+		let link = document.querySelectorAll("a[data-toggle='tab']");
 
 		function changebg(x) {
-			$(tab[x]).addClass("active-tab")
+			$(tab[x]).addClass("active-tab");
 			for (let i = 0; i < tab.length; i++) {
 				if (i != x) {
-					$(tab[i]).removeClass("active-tab")
+					$(tab[i]).removeClass("active-tab");
 				}
 			}
 		}
 
-
-		$(document).ready(function () {
+		$(document).ready(function() {
 			for (let i = 0; i < tab.length; i++) {
-				$(tab[i]).click(function () {
-					changebg(i)
-				})
+				$(tab[i]).click(function() {
+					changebg(i);
+				});
 			}
-		})
-
+		});
 	</script>
 </body>
