85e3da1d00d353412e9ac7667bd4fc1310df873c
adit <aeka489@gmail.com>
2019-06-26 04:03:54 +0700

    stuck

--- application/views/frontend/user_dashboard/section-setting.php
+++ application/views/frontend/user_dashboard/section-setting.php
@@ -1,42 +1,47 @@
-<div class="container w-50 shadows rounded-md pt-5 pb-3 pr-5 setting-float-left">
-	<div class="float-right mr-5">
-		<h3 class="text-white font-weight-bold">Your Details</h3>
-		<ul role="tablist">
-			<li class="list-unstyled ">
-				<a href="#profile" id="profiles-tab" class="text-white font-weight-bold nav-link active"
-					aria-selected="true" data-toggle="tab" role="tab">Profile</a>
-			</li>
-			<li class="list-unstyled">
-				<a href="#email" id="emails-tab" class="text-white font-weight-bold nav-link " aria-selected="false"
-					data-toggle="tab" role="tab">Email</a>
-			</li>
-		</ul>
-	</div>
-</div>
-<div class="mr-0 mb-5 ml-auto w-65 border rounded-sm border-2">
-
-	<!--  -->
-	<div class="tab-pane fade" id="profile" aria-labelledby="profiles-tab" role="tabpanel">
-		<div class="p-0">
-			<h3 class="ml-5 my-4 font-weight-bold">Public Profile</h3>
+<div>
+	<div class="container w-50 shadows rounded-md pt-5 pb-3 pr-5 setting-float-left">
+		<div class="float-right mr-5">
+			<h3 class="text-white font-weight-bold">Your Details</h3>
+			<ul role="tablist">
+				<li>
+					<a href="#profile" id="profile-tab" class="nav-link active" aria-selected="true" data-toggle="tab"
+						role="tab">Profile</a>
+				</li>
+				<li>
+					<a href="#email" id="email-tab" class="nav-link" aria-selected="false" data-toggle="tab"
+						role="tab">Email</a>
+				</li>
+			</ul>
 		</div>
-		<div class="p-5 border-top mb-5">
-			<h4 class="ml-4 font-weight-bold">Avatar</h4>
-			<div class="upload-box ">
-				<div class="hold w-75 px-3 py-2"><a href="#">No file chosen</a> <span class="btn-file py-1 px-4 btn">
-						Chose file
-						<input type="file">
-					</span> </div>
+	</div>
+
+	<div class="mr-0 mb-5 ml-auto w-65 border rounded-sm border-2">
+
+		<!--  -->
+		<div class="tab-content mb-5">
+			<div class="tab-pane fade show active" id="profile" aria-labelledby="profile-tab" role="tabpanel">
+
+				<div class="p-0">
+					<h3 class="ml-5 my-4 font-weight-bold">Public Profile</h3>
+				</div>
+				<div class="p-5 border-top mb-5">
+					<h4 class="ml-4 font-weight-bold">Avatar</h4>
+					<div class="upload-box ">
+						<div class="hold w-75 px-3 py-2"><a href="#">No file chosen</a> <span
+								class="btn-file py-1 px-4 btn">
+								Chose file
+								<input type="file">
+							</span> </div>
+					</div>
+				</div>
+			</div>
+
+			<div class="tab-pane fade" id="email" aria-labelledby="email-tab" role="tabpanel">
+				<p>email</p>
+
 			</div>
 		</div>
+
+
 	</div>
-
-
-	<!-- section download -->
-	<div class="tab-pane fade" id="email" aria-labelledby="emails-tab" role="tabpanel">
-		<div class="p-0">
-			<h3 class="ml-5 my-4 font-weight-bold">Public Profile</h3>
-		</div>
-	</div>
-
 </div>
